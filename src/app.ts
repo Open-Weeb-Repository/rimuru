3fef47ed62ca64edff8722461c6f5af4a64d0dd6
aquaswim <bayusetiawan37@gmail.com>
2020-08-14 21:33:21 +0700

    adding remove active scrap job when fail in certain threshold

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import debug from 'debug';
+import config from 'config';
 import projectJobsRepo from './repository/project-jobs';
 
 const log = debug('rimuru:app');
@@ -11,5 +12,19 @@ export class App {
     async start(){
         const activeJobs = await projectJobsRepo.getActiveJobs();
         log('%d active job found in database', activeJobs.length);
+        const task: Promise<any>[] = [];
+        for (const activeJob of activeJobs) {
+            if ( activeJob.n_done === 0 && activeJob.n_fail > config.get('projectNeverThreshold') ) {
+                // project not taken by provider
+                task.push(projectJobsRepo.setJobToInactive(activeJob));
+            } else if (activeJob.n_done > 0 && activeJob.n_fail > config.get('projectInactiveThreshold')) {
+                // project no longer active
+                task.push(projectJobsRepo.setJobToInactive(activeJob));
+            } else {
+                // send job to worker
+                log("send job %s to worker", activeJob._id.toString());
+            }
+        }
+        await Promise.all(task);
     }
 }
--- src/repository/project-jobs.ts
+++ src/repository/project-jobs.ts
@@ -5,11 +5,23 @@ import { FindResult } from "monk";
 
 export const projectJobs = db.get<IProjectJob & IDB.HasLasRun>("projectScrapeJobs");
 projectJobs.createIndex('state');
+projectJobs.createIndex('malId provider');
 
 export default {
     getActiveJobs () {
         return projectJobs.find({
             state: "active"
         }) as any as Promise<FindResult<IProjectJob & IDB.HasLasRun>>;
+    },
+
+    setJobToInactive (projectJob: IProjectJob) {
+        return projectJobs.update({
+            malId: projectJob.malId,
+            provider: projectJob.provider
+        }, {
+            $set: {
+                state: "inactive"
+            }
+        })
     }
 }
