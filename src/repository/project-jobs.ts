3782330906f6740893306654948eb7da3f26536c
aquaswim <bayusetiawan37@gmail.com>
2020-08-16 21:48:37 +0700

    paginate active jobs, and creating more bugs

--- src/repository/project-jobs.ts
+++ src/repository/project-jobs.ts
@@ -1,20 +1,22 @@
 import db from "../commons/db";
 import {IProjectJob} from "project-jobs";
 import {IDB} from "rimuru.commons";
-import { FindResult } from "monk";
+import {FindResult, FindOptions, WithID} from "monk";
 
-export const projectJobs = db.get<IProjectJob & IDB.HasLasRun>("projectScrapeJobs");
+type IDBType = WithID<IProjectJob & IDB.HasLasRun>;
+
+export const projectJobs = db.get<IDBType>("projectScrapeJobs");
 projectJobs.createIndex('state');
 projectJobs.createIndex('malId provider');
 
 export default {
-    getActiveJobs () {
+    getActiveJobs(options?: FindOptions): Promise<FindResult<IDBType>> {
         return projectJobs.find({
             state: "active"
-        }) as any as Promise<FindResult<IProjectJob & IDB.HasLasRun>>;
+        }, options);
     },
 
-    setJobToInactive (projectJob: IProjectJob) {
+    setJobToInactive(projectJob: IProjectJob) {
         return projectJobs.update({
             malId: projectJob.malId,
             provider: projectJob.provider
@@ -23,5 +25,47 @@ export default {
                 state: "inactive"
             }
         })
+    },
+
+    async walkInActiveJob(process: (projectJob: WithID<IDBType>) => Promise<boolean>, fetchSize = 100) {
+        const collectionCount = await projectJobs.count({
+            state: "active",
+        });
+        let now = 0;
+        const inactiveJobIds = [];
+        do {
+            const activeJobs = await this.getActiveJobs({
+                limit: fetchSize,
+                skip: now
+            });
+            now += activeJobs.length;
+            for (const activeJob of activeJobs) {
+                if (!await process(activeJob)) {
+                    // add to inactive
+                    inactiveJobIds.push(activeJob._id);
+                }
+            }
+        } while (collectionCount > now);
+        // todo: this is stupid, time to change from monk to mongodb driver or other
+        await Promise.all(
+            inactiveJobIds.map(
+                _id => projectJobs.update(
+                    {
+                        _id
+                    },
+                    {
+                        $set: {
+                            state: "inactive"
+                        }
+                    }
+                )
+            )
+        )
+
+        return {
+            totalSize: collectionCount,
+            processFail: inactiveJobIds.length,
+            processSuccess: collectionCount - inactiveJobIds.length
+        }
     }
 }
